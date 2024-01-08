0a860df4450c5dfe476bf8f0cfd0941fde40e161
boxkr <keeg9087@gmail.com>
2024-01-08 01:35:42 +0000

    create user object and migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/new'
+  get 'users/show'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/search'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
