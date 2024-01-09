68643612a40c10c956fce7c7954e6ffb79f7f0da
tyxfred <tyxfred@gmail.com>
2024-01-09 10:10:42 +0800

    initial landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root :to => 'pages#index'
+  
   get 'transactions/index'
   get 'transactions/new'
   get 'transactions/show'
@@ -21,7 +23,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-
+  
 
   # action -> controller#action
   get '/users/:uin/transfer', to: 'users#transfer', as: 'user-transfer'
