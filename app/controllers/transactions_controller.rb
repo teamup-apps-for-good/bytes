8b6fd8bb69eeeedeb61303abf3fa41e0610015d4
kelvinzheng3317 <kelvinzheng21@gmail.com>
2024-01-09 09:38:04 +0800

    set up basic receive code using session

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -19,4 +19,20 @@ class TransactionsController < ApplicationController
 
   def search
   end
+
+  def receive
+    @curr_user = User.find(session["uin"])
+    @amount = params[:num_credits]
+    
+    # NOTE: Pool.quantity not implemented yet
+    if @amount > Pool.quantity
+      flash[:warning] = "Not enough credits available, only #{Pool.quantity} credits currently available"
+      return -1   # keep user on same page so they can quickly change again
+    end
+
+    Transaction.new(:amount => @amount, :type => 'Receive')
+    @curr_user.add_credits(@amount)
+    flash[:notice] = "#{@amount} Credits received"
+    redirect_to profile_path
+  end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,8 @@
 class Transaction < ApplicationRecord
+  def initialize amount, type
+    @uin = session["uin"]
+    @amount = amount
+    @type = type
+    @timestamp = Time.now # NOTE: may need to reformat time later
+  end
 end
