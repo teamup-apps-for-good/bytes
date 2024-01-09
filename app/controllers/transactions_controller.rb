394cd9bb3ba7770f3e8e22b58ab3c203eeb7fa77
Nicholas <nickl239@att.net>
2024-01-09 10:53:34 +0800

    added transactions view and controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,11 +1,14 @@
 class TransactionsController < ApplicationController
   def index
+    @transactions = Transaction.all
   end
 
   def new
   end
 
   def show
+    id = params[:id]
+    @transaction = Transaction.find(id)
   end
 
   def create
@@ -18,5 +21,13 @@ class TransactionsController < ApplicationController
   end
 
   def search
+  end
+
+  def destroy
+    id = params[:id]
+    @transaction = Transaction.find(id)
+    @transaction.destroy
+    flash[:notice] = "Transaction deleted."
+    redirect_to transactions_path
   end
 end
