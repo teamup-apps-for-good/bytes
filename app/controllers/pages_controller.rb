6c6229c605e685d1582f4cdf67716ea4dbc52104
kaijiechen2002 <kaijiechen2002@tamu.edu>
2024-01-09 21:18:28 -0600

    Fixed small redirection issues within account creation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :require_login, only: [:index]
   def index
+    session[:creating] = false
     if logged_in?
       redirect_to user_path(@current_user), notice: 'Welcome, back!'
     end
