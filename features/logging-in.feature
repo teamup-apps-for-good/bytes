f0f34f0d1560db12f90855854c4d42d3ffa3100b
bryanyantx <bryanyan.tx@gmail.com>
2024-01-11 22:21:09 +0800

    add step

--- features/logging-in.feature
+++ features/logging-in.feature
@@ -5,6 +5,7 @@ Feature: logging in
     I want the ability to sign into Bytes using my verified school email address
 
 Scenario: user logs in with TAMU email
-    Given I am on the login page
-    When I click on the "Login with Google" button
+    Given I am a user with a TAMU email
+    When I am on the login page
+    And I click on the "Login with Google" button
     Then I should be logged in successfully
--- features/step_definitions/logging-in.rb
+++ features/step_definitions/logging-in.rb
@@ -1,14 +1,17 @@
-Given('I am on the login page') do
-  visit root_path
-end
-
-When('I click on the {string} button') do |string|
+Given('I am a user with a TAMU email') do
     OmniAuth.config.test_mode = true
     OmniAuth.config.add_mock(
         :google_oauth2,
         uid: '123456',
         info: { email: 'example@tamu.edu' }
     )
+end
+  
+When('I am on the login page') do
+  visit root_path
+end
+
+And('I click on the {string} button') do |string|
     click_on string
 end
 
