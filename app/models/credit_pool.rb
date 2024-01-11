654fc7679f070b630c14968c9f742e91df2f5620
kelvinzheng3317 <kelvinzheng21@gmail.com>
2024-01-11 23:52:16 +0800

    resolve style concerns

--- app/models/credit_pool.rb
+++ app/models/credit_pool.rb
@@ -3,18 +3,14 @@
 # class that represents the pool of donated credits available
 class CreditPool < ApplicationRecord
   validates :credits, presence: true
-  
+
   def add_credits(amt)
-
-      self.update(credits: self.credits + amt)
-
+    update(credits: credits + amt)
   end
 
   def subtract_credits(amt)
-    if (amt > self.credits)
-      raise Exception.new "Not enough credits to subtract this amount"
-    end 
-    
-    self.update(credits: self.credits - amt)
+    raise StandardError, 'Not enough credits to subtract this amount' if amt > credits
+
+    update(credits: credits - amt)
   end
 end
--- spec/controllers/credit_pools_controller_spec.rb
+++ spec/controllers/credit_pools_controller_spec.rb
@@ -1,12 +1,15 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe CreditPoolsController, type: :controller do
   before(:all) do
     User.destroy_all
     CreditPool.destroy_all
-    CreditPool.create({credits: 1})
+    CreditPool.create({ credits: 1 })
 
-    user = User.create(name: 'John', uin: '123456', email: 'j@tamu.edu', credits: '50', user_type: 'donor', date_joined: '01/01/2022')
+    user = User.create(name: 'John', uin: '123456', email: 'j@tamu.edu', credits: '50', user_type: 'donor',
+                       date_joined: '01/01/2022')
     @user = user
     @id = user.id
     @name = user.name
@@ -16,77 +19,68 @@ RSpec.describe CreditPoolsController, type: :controller do
     @uin = user.uin
     OmniAuth.config.test_mode = true
     OmniAuth.config.add_mock(
-        :google_oauth2,
-        uid: user.id,
-        info: { email: user.email }
+      :google_oauth2,
+      uid: user.id,
+      info: { email: user.email }
     )
-
-    
-    
-    
   end
 
-  #not sure how relevant the code for credit pools is, we're never really doing anything with them directly accessed through routes...
-  #a lot of this code will be cookie-cutter from the testing PA with regards to like create, update, delete, etc
+  # not sure how relevant the code for credit pools is, we're never really doing anything with them directly accessed through routes...
+  # a lot of this code will be cookie-cutter from the testing PA with regards to like create, update, delete, etc
 
-    describe 'creates' do
-        it 'credit pool normal' do
-
-        session[:user_id] = @user.id
-        post :create, params: {credit_pool: {credits: 1}}
-        expect(response).to have_http_status(:redirect)
-        CreditPool.find_by(credits: 1).destroy
-        end
-
-        it 'credit pool bad' do
-            pool = CreditPool.create({credits: 1})
-            session[:user_id] = @user.id
-            post :create, params: {credit_pool: {test: 1}}
-        end
+  describe 'creates' do
+    it 'credit pool normal' do
+      session[:user_id] = @user.id
+      post :create, params: { credit_pool: { credits: 1 } }
+      expect(response).to have_http_status(:redirect)
+      CreditPool.find_by(credits: 1).destroy
     end
 
-    describe 'destroys' do
-        it 'destroys normally' do
-        pool = CreditPool.create({credits: 1})
-        session[:user_id] = @user.id
-        delete :destroy, params: { id: pool.id }
-        expect(response).to have_http_status(:redirect)
-        end
+    it 'credit pool bad' do
+      CreditPool.create({ credits: 1 })
+      session[:user_id] = @user.id
+      post :create, params: { credit_pool: { test: 1 } }
+    end
+  end
+
+  describe 'destroys' do
+    it 'destroys normally' do
+      pool = CreditPool.create({ credits: 1 })
+      session[:user_id] = @user.id
+      delete :destroy, params: { id: pool.id }
+      expect(response).to have_http_status(:redirect)
+    end
+  end
+
+  describe 'updates' do
+    it 'graceful' do
+      pool = CreditPool.create({ credits: 1 })
+      session[:user_id] = @user.id
+      patch :update, params: { id: pool.id, credit_pool: { credits: 2 } }
+
+      expect(response).to have_http_status(:redirect)
     end
 
-    describe 'updates' do
-        it 'graceful' do
-            pool = CreditPool.create({credits: 1})
-            session[:user_id] = @user.id
-            patch :update, params: { id: pool.id, credit_pool: {credits: 2 }}
-
-            expect(response).to have_http_status(:redirect)
-        end
-
-        it 'not graceful' do
-            pool = CreditPool.create({credits: 1})
-            session[:user_id] = @user.id
-            patch :update, params: { id: pool.id, credit_pool: {credits: nil }}
-        end
+    it 'not graceful' do
+      pool = CreditPool.create({ credits: 1 })
+      session[:user_id] = @user.id
+      patch :update, params: { id: pool.id, credit_pool: { credits: nil } }
     end
+  end
 
-    describe 'index' do
-    
-        it 'hits index' do
-            session[:user_id] = @user.id
-            get :index
-            expect(response).to have_http_status(:success)
-        end
+  describe 'index' do
+    it 'hits index' do
+      session[:user_id] = @user.id
+      get :index
+      expect(response).to have_http_status(:success)
     end
+  end
 
-    describe 'new' do
-        it 'hits index' do
-            session[:user_id] = @user.id
-            get :new
-            expect(response).to have_http_status(:success)
-        end
+  describe 'new' do
+    it 'hits index' do
+      session[:user_id] = @user.id
+      get :new
+      expect(response).to have_http_status(:success)
     end
-
-
-
+  end
 end
