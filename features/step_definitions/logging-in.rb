58bfb566b809e8df1835273ffd29fa4ac2a81bc2
bryanyantx <bryanyan.tx@gmail.com>
2024-01-11 10:47:01 +0800

    add step definition snippets

---
+++ features/step_definitions/logging-in.rb
@@ -0,0 +1,15 @@
+Given('I am on the login page') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I click on the {string} button') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I log in with a tamu.edu email') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should be logged in successfully') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
