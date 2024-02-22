d0d5354b5707b3f098173fc7fea435c3995d40ba
boxkr <keeg9087@gmail.com>
2024-02-22 05:12:04 +0000

    re-adding cucumber

---
+++ features/step_definitions/homepage-metrics.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+Given('I am not logged in') do
+    expect(@user).to eq nil
+end
+  
+Given('I am on the home page') do
+    visit('/')
+end
+
+Then('I should see a valid metric displayed on screen') do
+    
+    element = find('.metric-text')
+    puts element
+    expect(element.text).not_to be_empty
+end
+  
